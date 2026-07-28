cc1244e7277c6cfaeee02ba01a1a0f1bbc5e3f5c
Miguel Maza <maza68206@gmail.com>
2026-07-28 11:39:26 -0500

    Implementacion models

---
+++ src/models/MapPoint.java
@@ -0,0 +1,48 @@
+package models;
+
+public class MapPoint {
+
+    private String id;
+    private int x;
+    private int y;
+
+
+    public MapPoint(String id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    
+    
+}
---
+++ src/models/VisualizationMode.java
@@ -0,0 +1,9 @@
+
+package models;
+
+
+public enum VisualizationMode {
+
+    EXPLORATION,
+    FINAL_PATH
+}
